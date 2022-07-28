d9c4dd0e258bef7a1f41a95fe04e8af30d714ade
rashedsabbir <rashedulhasan1996@gmail.com>
2022-07-28 12:54:15 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Pages/Home/Header/Navbar';
 import Footer from './Pages/Home/Footer/Footer';
 import Home from './Pages/Home/Home';
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+
 
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <Home></Home>
-      <Footer></Footer>
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+      
+      <Footer/>
+      </Router>
     </div>
   );
 }
