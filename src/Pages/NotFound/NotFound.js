c6f07471a1d97ebf42180cec54b2e9b2994f4bd4
rashedsabbir <rashedulhasan1996@gmail.com>
2022-08-10 00:39:23 +0600

    dashboard removed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -12,7 +12,7 @@ const NotFound = () => {
                                 doorway to the great nothing
                             </h1>
                             <p class="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                            <button class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"><a href="/"> Take me there!</a></button>
+                            <button class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-rose-400 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"><a href="/"> Take me there!</a></button>
                         </div>
                     </div>
                     <div>
