c1bf70b523630020bee324fd087c2de036e9f19f
rashedsabbir <rashedulhasan1996@gmail.com>
2022-08-17 12:21:09 +0600

    google refer policy added

--- src/Pages/CustomerReview/CustomerReview.js
+++ src/Pages/CustomerReview/CustomerReview.js
@@ -50,7 +50,7 @@ const CustomerReview = () => {
      displayReviews.map(review=><SwiperSlide key={review.id}>
     <div className="relative text-center bg-amber-100 shadow-xl justify-center space-y-4 rounded-md bg-gray-900 p-10 text-white "> <span className="absolute left-3/4 top-16  text-3xl text-gray-600">{quoteRight}</span>
     <span className="absolute left-10 top-3/4 text-3xl text-gray-600">{quoteLeft}</span>
-   <div className="flex justify-center "> <div className="w-24 border-2 border-red-500 h-24 rounded-full   overflow-hidden"> <img src={review.image} alt="" /></div></div>
+   <div className="flex justify-center "> <div className="w-24 border-2 border-red-500 h-24 rounded-full   overflow-hidden"> <img src={review.image} referrerpolicy="no-referrer" alt="" /></div></div>
       <h2 className='text-4xl text-black'>{review.name}</h2>
       <p className='p-5 text-rose-600'>{review.description}</p>
       <div className="flex justify-center">
